16c771b838f6c7084d3ab09c298a41dc762c6d01
AdithamaMulia <130443704+AdithamaMulia@users.noreply.github.com>
2024-10-23 17:31:57 +0700

    udh gw benerin o7

--- app/src/main/java/com/example/utsmap/MainActivity.kt
+++ app/src/main/java/com/example/utsmap/MainActivity.kt
@@ -2,24 +2,35 @@ package com.example.utsmap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.navigation.NavigationView
+import com.google.firebase.FirebaseApp
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.loginFragment, R.id.signupFragment),
+            findViewById(R.id.drawer_layout)
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
